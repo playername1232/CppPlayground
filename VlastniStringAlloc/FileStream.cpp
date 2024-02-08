e521737b52cd13e749fc75826cff53d9bb4551d5
playername1232 <martinkrepcik@gmail.com>
2024-02-08 13:20:45 +0100

    [FileStream] Added Declaration for Read and Write methods

--- VlastniStringAlloc/FileStream.cpp
+++ VlastniStringAlloc/FileStream.cpp
@@ -1,1 +1,35 @@
 #include "FileStream.h"
+#include "CustomMacros.h"
+#include <iostream>
+#include <fstream>
+
+char* FileStream::ReadAllText(char* filePath)
+{
+    // Alocate per 500 bytes. 
+    // With each 501th character allocate another 500.
+    // After reading is done, resize the pointer to string's length
+
+    char* buffer = (char*)allocate_heap_clean(500, 1);
+
+    return nullptr;
+}
+
+char** FileStream::ReadAllLines(char* filePath)
+{
+    // Alocate per 500 bytes for each line. 
+    // With each 501th character allocate another 500 to the line.
+    // After reading is done, resize the pointer to file line according to string's length
+    // Always allocate one line by one. Next line = alloc += 1;
+
+    return nullptr;
+}
+
+void FileStream::WriteAllText(char* filePath, char* text)
+{
+
+}
+
+void FileStream::WriteAllLines(char* filePath, char** content)
+{
+
+}
--- VlastniStringAlloc/FileStream.h
+++ VlastniStringAlloc/FileStream.h
@@ -1,6 +1,10 @@
 #pragma once
 class FileStream
 {
+	char* ReadAllText(char* filePath);
+	char** ReadAllLines(char* filePath);
 
+	void WriteAllText(char* filePath, char* text);
+	void WriteAllLines(char* filePath, char** content);
 };
 
