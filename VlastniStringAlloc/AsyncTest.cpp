475016270d66ac856d7c9e8e186cd399c48900f5
playername1232 <41285762+playername1232@users.noreply.github.com>
2024-02-08 20:30:01 +0100

    [AsyncTest] Added simple Asynchronous tests for practice

--- VlastniStringAlloc/AsyncTest.cpp
+++ VlastniStringAlloc/AsyncTest.cpp
@@ -1,1 +1,79 @@
 #include "AsyncTest.h"
+
+using namespace std;
+using namespace std::chrono_literals;
+
+void AsyncTest::Test1()
+{
+	future<void> launch2nd = async(launch::async, &AsyncTest::Test2, this);
+
+	for (int i = 0; i < 500; i++)
+	{
+		std::cout << "Test1() i = " << i << std::endl;
+		this_thread::sleep_for(chrono::milliseconds(50));
+	}
+
+	cout << endl << endl << "Test1() finished! Waiting for Test2()" << endl << endl;
+
+	launch2nd.wait();
+
+	cout << endl << endl << "Test1() call: Test2() finished!" << endl << endl;
+	
+	cout << endl << endl << "Test3() laucnhed!" << endl << endl;
+
+	future<void> launch3rd = async(launch::async, &AsyncTest::Test3, this);
+
+	cout << endl << endl << "Test1() exit reached!" << endl << endl;
+}
+
+void AsyncTest::Test2()
+{
+	for (int i = 0; i < 400; i++)
+	{
+		std::cout << "Test2() i = " << i << std::endl;
+		this_thread::sleep_for(chrono::milliseconds(100));
+	}
+
+	cout << endl << endl << "Test2() finished!" << endl << endl;
+}
+
+void AsyncTest::Test3()
+{
+	bool launched = false;
+	for (int i = 0; i < 300; i++)
+	{
+		std::cout << "Test3() i = " << i << std::endl;
+		this_thread::sleep_for(chrono::milliseconds(100));
+
+		if (i == 150 && !launched)
+		{
+			launched = true;
+			future<void> launch4th = async(launch::async, &AsyncTest::Test4, this);
+
+			cout << endl << endl << "Test3() launched Test4() waiting for exit" << endl << endl;
+			launch4th.wait();
+			cout << endl << endl << "Test3() call: Test4() finished! Continuing in Test3()" << endl << endl;
+		}
+	}
+
+	cout << endl << endl << "Test3() finished!" << endl << endl;
+}
+
+void AsyncTest::Test4()
+{
+	for (int i = 0; i < 200; i++)
+	{
+		std::cout << "Test4() i = " << i << std::endl;
+		this_thread::sleep_for(chrono::milliseconds(50));
+	}
+
+	cout << endl << endl << "Test4() finished!" << endl << endl;
+}
+
+void AsyncTest::GeneralTest()
+{
+	future<void> future1 = async(launch::async, &AsyncTest::Test1, this);
+	future1.wait();
+
+	cout << endl << endl << "GeneralTest() call: All tests finished!!!" << endl << endl;
+}
--- VlastniStringAlloc/AsyncTest.h
+++ VlastniStringAlloc/AsyncTest.h
@@ -1,4 +1,16 @@
 #pragma once
+#include <iostream>       // std::cout
+#include <functional>     // std::ref
+#include <thread>         // std::thread
+#include <future>         // std::promise, std::future
+
 class AsyncTest
 {
+public:
+	void Test1();
+	void Test2();
+	void Test3();
+	void Test4();
+
+	void GeneralTest();
 };
