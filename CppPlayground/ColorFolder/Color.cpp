d70cc6c1bbd013ed279e3618bc89afc91bde6add
playername1232 <41285762+playername1232@users.noreply.github.com>
2024-03-27 20:52:46 +0100

    [Color] Moved constructor definitions to cpp file

--- CppPlayground/ColorFolder/Color.cpp
+++ CppPlayground/ColorFolder/Color.cpp
@@ -3,6 +3,24 @@
 
 class DynamicStringLibrary;
 
+Color::Color()
+{
+    // Set all to 0
+    red = green = blue = 0;
+}
+
+Color::Color(int red, int green, int blue)
+{
+    this->red = (red >= 0 && red <= 255) ? red : 0;
+    this->green = (green >= 0 && green <= 255) ? green : 0;
+    this->blue = (blue >= 0 && blue <= 255) ? blue : 0;
+}
+
+Color::Color(char* hexCode)
+{
+    
+}
+
 char* Color::GetHexCode()
 {
     DynamicStringLibrary res = "#";
--- CppPlayground/ColorFolder/Color.h
+++ CppPlayground/ColorFolder/Color.h
@@ -7,24 +7,10 @@ private:
     int green;
     int blue;
 public:
-    Color()
-    {
-        // Set all to 0
-        red = green = blue = 0;
-    }
-
-    Color(int red, int green, int blue)
-    {
-        this->red = (red >= 0 && red <= 255) ? red : 0;
-        this->green = (green >= 0 && green <= 255) ? green : 0;
-        this->blue = (blue >= 0 && blue <= 255) ? blue : 0;
-    }
-
+    Color();
+    Color(int red, int green, int blue);
     // 2b done
-    Color(char* hexCode)
-    {
-        
-    }
+    Color(char* hexCode);
     
     char* GetHexCode();
 };
